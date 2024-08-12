012bd74e7113be8909c993cf586ca49121bf091b
Ajay Kumar <ajay@careinsight.ai>
2024-08-12 23:39:44 +0530

    changed code

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-x = int(input("Enter a number"))
-print(x)
+x = int(input("Enter a number:\n"))
+rev = 0
+while x > 0:
+    rev = rev * 10 + x % 10
+    print(rev)
+    x = x // 10
+    print(x)
+print("Reversed number", rev)
